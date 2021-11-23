64625b901d0aed18ca676ec041f0f1136e56ed13
siCastaProjectos <sicastaprojectos@gmail.com>
2021-11-22 19:56:07 -0500

    actualizado

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
-});
-
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
 });
@@ -17,4 +13,8 @@ app.get('/login', (req,res)=>{
 
 app.get('/register', (req,res)=>{
     res.sendFile(__dirname + '/views/register.html');
+});
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log('Servidor funcionando');
 });
